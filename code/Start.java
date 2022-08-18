7f7fe4647614749d20029c08f5eeba7bccb80044
Voragun Supawong <voragun2543@gmail.com>
2022-08-18 20:17:41 +0700

    Summation of integer in array

--- code/Start.java
+++ code/Start.java
@@ -1,27 +1,37 @@
 class Start {
-	public static void main(String[] data) {
-		Car c = new Car(); // create instance
-		System.out.println( c instanceof Car ); // true
-                
-                // Given an array of Vehicle, write code fragment to
-                // count the number of instance of Car
-                Vehicle[] list = { new Car(), new Truck(), new Car(), new Car(),
-                                    new Truck(), new Car(), new Car(), new Car()};
-                int count = 0;
-                for (int i = 0; i < list.length; i++) {
-                    // System.out.println(list[i] instanceof Car);
-                    if(list[i] instanceof Car) {
-                        count++;
-                    }
-                }
-                System.out.println(count);
-	}
+
+    public static void main(String[] data) {
+        int n = 7;
+        // Integer w = new Integer(n);  // Deprecated
+        
+        Integer w = n;   // autoboxing
+        int m = w;       // unboxing
+        
+        Object[] all = { 37.5, 38, 37.2, 37, 36.7,
+                         36, 37, 37.2, 38, 37.3, 35.7 };
+        
+        // Write code to find the summation of integers
+        // from the given array
+        int result = 0;
+        for (int i = 0; i < all.length; i++) {
+            if(all[i] instanceof Integer meow) {
+                result += meow;            
+            }
+        }
+         System.out.print(" " + result);
+        
+    }
 }
 
 class Vehicle {
     double price;
 }
+
 class Car extends Vehicle { }
+
+@Deprecated
 class Boat extends Vehicle { }
+
 class Truck extends Vehicle { }
-class Scooter { }
+
+class Scooter { } // super class of Scooter is "Object"
