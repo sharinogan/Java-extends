60616151330fcf84b0f84f159f03f1516aa91d6b
Voragun Supawong <voragun2543@gmail.com>
2022-08-18 21:19:48 +0700

    CPU

--- code/Start.java
+++ code/Start.java
@@ -6,7 +6,7 @@ class Start {
     public static void main(String[] data) {        
         Player[] all = {
             new Player("David", 7, 45000.0),
-            new Player("Michael", 10, 40000.0),
+            new Player("Michael", 5, 40000.0),
             new Player("Messi", 10, 43000.0),
             new Player("Peter", 1, 42000.0)
         };
@@ -21,9 +21,7 @@ class PlayerCompare implements Comparator {
     public int compare(Object x, Object y) {
         Player a = (Player)x;
         Player b = (Player)y;
-        if(a.salary < b.salary) return -1;
-        if(a.salary > b.salary) return +1;
-        return 0;
+        return a.number - b.number;
     }
 }
 
