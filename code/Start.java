1afafb9faf77933a8b5c41a5572837c6d0e6b11f
Voragun Supawong <voragun2543@gmail.com>
2022-08-18 20:50:23 +0700

    abstract class

--- code/Start.java
+++ code/Start.java
@@ -1,27 +1,16 @@
 class Start {
 
     public static void main(String[] data) {
-        int n = 7;
-        // Integer w = new Integer(n);  // Deprecated
-        
-        Integer w = n;   // autoboxing
-        int m = w;       // unboxing
-        
-        Object[] all = { 37.5, 38, 37.2, 37, 36.7,
-                         36, 37, 37.2, 38, 37.3, 35.7 };
-        
-        // Write code to find the summation of integers
-        // from the given array
-        int result = 0;
-        for (int i = 0; i < all.length; i++) {
-            if(all[i] instanceof Integer) {
-                result += (Integer)all[i];            
-            }
-        }
-         System.out.print(" " + result);
-        
+        // User u = new User();      // Error
+        User u = new Monthly();      // OK
+        //Monthly m = new User();      // Error
+        Monthly m = new Monthly();   // OK
     }
 }
+
+abstract class User { }
+
+class Monthly extends User { }
 
 class Vehicle {
     double price;
