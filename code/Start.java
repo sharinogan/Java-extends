6fece0c7d5c1a5058765b5da72524bcf8e3292c2
Voragun Supawong <voragun2543@gmail.com>
2022-08-18 21:13:07 +0700

    Compare value in array

--- code/Start.java
+++ code/Start.java
@@ -1,26 +1,40 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Start {
 
-    public static void main(String[] data) {
-        // User u = new User();      // Error
-        User u = new Monthly();      // OK
-        //Monthly m = new User();      // Error
-        Monthly m = new Monthly();   // OK
+    public static void main(String[] data) {        
+        Player[] all = {
+            new Player("David", 7, 45000.0),
+            new Player("Michael", 10, 40000.0),
+            new Player("Messi", 10, 43000.0),
+            new Player("Peter", 1, 42000.0)
+        };
+        Arrays.sort(all, new PlayerCompare());
+        for (Player player : all) {
+            System.out.println(player.name);
+        }
     }
 }
 
-abstract class User { }
-
-class Monthly extends User { }
-
-class Vehicle {
-    double price;
+class PlayerCompare implements Comparator {
+    public int compare(Object x, Object y) {
+        Player a = (Player)x;
+        Player b = (Player)y;
+        if(a.salary < b.salary) return -1;
+        if(a.salary > b.salary) return +1;
+        return 0;
+    }
 }
 
-class Car extends Vehicle { }
-
-@Deprecated
-class Boat extends Vehicle { }
-
-class Truck extends Vehicle { }
-
-class Scooter { } // super class of Scooter is "Object"
+class Player {
+    String name;
+    int number;
+    double salary;
+    
+    Player (String name, int number, double salary) {
+        this.name = name;
+        this.number = number;
+        this.salary = salary;
+    }
+}
